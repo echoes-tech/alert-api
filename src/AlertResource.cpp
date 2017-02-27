771bcd63693978e7cd959c52cc6a9342d512830a
Thomas <thomas@echoes.fr>
2017-02-27 15:03:26 +0100

    opensourcing : changing headers for cpp files

--- src/AlertResource.cpp
+++ src/AlertResource.cpp
@@ -1,15 +1,28 @@
-/* 
- * API AlertResource
- * @author ECHOES Technologies (GDR)
- * @date 05/02/2013
- * 
- * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO ECHOES TECHNOLOGIES SAS
- * AND MAY NOT BE REPRODUCED, PUBLISHED OR DISCLOSED TO OTHERS WITHOUT
- * COMPANY AUTHORIZATION.
- * 
- * COPYRIGHT 2013 BY ECHOES TECHNOLGIES SAS
+/**
+ * @file AlertResource.cpp
+ * @author Thomas Saquet, Florent Poinsaut
+ * @date 
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Alert - API is a part of the Alert software
+ * Copyright (C) 2013-2017
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  * 
  */
+
 #include "AlertResource.h"
 
 using namespace std;
@@ -1037,4 +1050,3 @@ EReturnCode AlertResource::processDeleteRequest(const Wt::Http::Request &request
 
     return res;
 }
-
